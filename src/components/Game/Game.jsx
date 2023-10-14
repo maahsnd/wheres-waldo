fa032b2c516d86784895e4102fa2f936e9aadcc8
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-14 14:31:08 -0700

    Implement target box

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './game.module.css?inLine';
 
 function Game() {
+  const [targetBox, setTargetBox] = useState(null);
+
   const target = (e) => {
-    //dev img size is 1300 x 800
-    console.log(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+    const image = e.currentTarget;
+    const x = e.clientX;
+    const y = e.clientY;
+
+    // Create a new box around the click coordinates
+    setTargetBox({
+      left: x - 35,
+      top: y - 35
+    });
   };
 
   return (
     <div className={styles.gameContainer}>
       <div className={styles.imageContainer} onClick={target}>
         <img src="/waldo.jpeg" alt="Game Image" className={styles.gameImage} />
+        {targetBox && (
+          <div
+            className={styles.box}
+            style={{ left: targetBox.left + 'px', top: targetBox.top + 'px' }}
+          ></div>
+        )}
       </div>
     </div>
   );
