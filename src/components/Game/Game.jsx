97b64727ab20c3029da429b05b49f8abcc32dcf1
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-14 13:42:26 -0700

    Get coordinates on click

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import styles from './game.module.css?inLine';
 
 function Game() {
+  const target = (e) => {
+    console.log(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+  };
+
   return (
     <div className={styles.gameContainer}>
-      <div className={styles.imageContainer}>
+      <div className={styles.imageContainer} onClick={target}>
         <img src="/waldo.jpeg" alt="Game Image" className={styles.gameImage} />
       </div>
     </div>
