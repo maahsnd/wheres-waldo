98fd7e926f77c1e1d53e2645a2633a61217bf77a
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-17 14:52:58 -0700

    Connect to backend Create submit selection function. Insert if conditions to react to backend response. Convert option change to async to prevent function from proceeding while promise settles.

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -24,49 +24,73 @@ function Game() {
         console.error('error fetching game');
       }
       const data = await response.json();
-      setGame({ img_link: data.img_link, characters: data.characters });
+      setGame({
+        img_link: data.img_link,
+        characters: data.characters,
+        gameId: data.gameId
+      });
       setLoading(false);
     }
     fetchGame();
   }, []);
 
-  const handleOptionChange = (e) => {
+  const submitSelection = async (selection) => {
+    const response = await fetch(`http://localhost:3000/games/${name}/coords`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        character: selection,
+        coords: markerCoords,
+        gameId: game.gameId
+      })
+    });
+    if (!response.ok) {
+      throw new Error('Selection submission failed');
+    }
+    const data = await response.json();
+    return data;
+  };
+
+  const handleOptionChange = async (e) => {
     setSelectedOption(e.target.value);
     //submit to backend
-
+    const response = await submitSelection(e.target.value);
     //fill in proper if conditions in response to backend
-    //success
-    if (false) {
+    //successful guess
+    if (response.found) {
       setFoundMarkers([...foundMarkers, markerCoords]);
     }
-    //failure
-    if (false) {
+    //unsuccessful guess
+    if (!response.found) {
       setError({ msg: `Sorry, that's not ${e.target.value}` });
       setTimeout(() => {
         setError(null);
       }, 2000);
     }
     //win
-    if (false) {
-      setActive(true);
+    if (response.found && response.win) {
       setWin(true);
     }
 
-    /* setActive(false); */
+    setActive(false);
     setTargetBox(null);
     setDropdown(null);
     setSelectedOption('');
   };
 
   const target = (e) => {
+    //disable function during character selection
+    if (active) {
+      return;
+    }
     // Calculate the coordinates relative to the image dimensions
     const image = document.querySelector(`.${styles.gameImage}`);
     const x = e.clientX - image.getBoundingClientRect().left;
     const y = e.clientY - image.getBoundingClientRect().top;
-    //disable during character selection
-    if (active) {
-      return;
-    }
+    console.log('x ' + x, 'y ' + y);
+
     setMarkerCoords({
       x: x,
       y: y
@@ -81,6 +105,9 @@ function Game() {
       left: e.clientX - 50,
       top: e.clientY - 45
     });
+    //disable after first click so opening select menu
+    //does not register new coordinates
+    setActive(true);
   };
 
   if (loading)
@@ -135,11 +162,7 @@ function Game() {
               className={styles.dropdown}
               style={{ left: dropdown.left + 'px', top: dropdown.top + 'px' }}
             >
-              <select
-                value={selectedOption}
-                onChange={handleOptionChange}
-                onClick={() => setActive(true)}
-              >
+              <select value={selectedOption} onChange={handleOptionChange}>
                 <option value="">Character</option>
                 {game.characters.map((character, index) => (
                   <option key={index} value={character}>
