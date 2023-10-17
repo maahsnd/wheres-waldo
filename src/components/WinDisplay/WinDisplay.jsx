47de4abe07dd47ab1fdfbbea55ee5624f4059511
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-17 16:05:02 -0700

    Fetch high scores on win

--- src/components/WinDisplay/WinDisplay.jsx
+++ src/components/WinDisplay/WinDisplay.jsx
@@ -1,21 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import styles from './win-display.module.css';
+import { useParams } from 'react-router-dom';
 
 const WinDisplay = () => {
   const [scores, setScores] = useState([]);
   const [userInput, setUserInput] = useState('');
   const [winTime, setWinTime] = useState(0);
   const [disableForm, setDisableForm] = useState(null);
+  const { name } = useParams();
 
-  useEffect(() => {
+  const getScores = async () => {
+    const response = await fetch(`http://localhost:3000/games/${name}/scores`);
+    if (!response.ok) {
+      throw new Error('Scores fetch failed');
+    }
+    const data = await response.json();
+    return data;
+  };
+
+  useEffect(async () => {
     // Simulated data for demonstration
     //fetch scores, time
-    const sampleScores = [
-      { name: 'Player 1', time: 45 },
-      { name: 'Player 2', time: 52 },
-      { name: 'Player 3', time: 60 }
-    ];
-    setScores(sampleScores);
+    const scores = await getScores();
+    setScores(scores);
     setWinTime(42); // Sample win time
   }, []);
 
